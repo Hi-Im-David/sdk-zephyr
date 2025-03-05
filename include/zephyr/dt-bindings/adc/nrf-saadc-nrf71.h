6ff7c3656f46015b6f98f5959266bc1edb8c1f04
David Jewsbury <david.jewsbury@nordicsemi.no>
2025-03-05 09:36:42 +0000

    drivers: adc: Update adc driver to support nRF7120 device
    Expands driver to cover nRF7120 ADC supply inputs. Also adds
    dt-bindings needed to define supply enums.
    
    Signed-off-by: David Jewsbury <david.jewsbury@nordicsemi.no>

---
+++ include/zephyr/dt-bindings/adc/nrf-saadc-nrf71.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_SAADC_NRF71_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_SAADC_NRF71_H_
+
+#include <zephyr/dt-bindings/adc/nrf-saadc.h>
+
+#define NRF_SAADC_VDDAO1V8 9
+#define NRF_SAADC_VDDAO0V8 10
+#define NRF_SAADC_VDDRF 11
+#define NRF_SAADC_VBAT 12
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_SAADC_NRF71_H_ */
