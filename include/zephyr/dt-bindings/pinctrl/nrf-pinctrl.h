bc345768b5c546fcceb363109d6ddca0958f3600
David Jewsbury <david.jewsbury@nordicsemi.no>
2025-08-13 15:05:40 +0100

    rename nrf-qspi2 to nrf-mspi

--- include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h
+++ include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h
@@ -242,18 +242,18 @@
 #define NRF_FUN_TDM_SDOUT        76U
 /** TDM MCK */
 #define NRF_FUN_TDM_MCK          77U
-/** QSPI version 2 Chip select */
-#define NRF_FUN_QSPI2_CSN        78U
-/** QSPI version 2 Clock pin */
-#define NRF_FUN_QSPI2_SCK        79U
-/** QSPI version 2 Data pin 0 */
-#define NRF_FUN_QSPI2_DQ0        80U
-/** QSPI version 2 Data pin 1 */
-#define NRF_FUN_QSPI2_DQ1        81U
-/** QSPI version 2 Data pin 2 */
-#define NRF_FUN_QSPI2_DQ2        82U
-/** QSPI version 2 Data pin 3 */
-#define NRF_FUN_QSPI2_DQ3        83U
+/** MSPI version 2 Chip select */
+#define NRF_FUN_MSPI_CSN         78U
+/** MSPI version 2 Clock pin */
+#define NRF_FUN_MSPI_SCK         79U
+/** MSPI version 2 Data pin 0 */
+#define NRF_FUN_MSPI_DQ0         80U
+/** MSPI version 2 Data pin 1 */
+#define NRF_FUN_MSPI_DQ1         81U
+/** MSPI version 2 Data pin 2 */
+#define NRF_FUN_MSPI_DQ2         82U
+/** MSPI version 2 Data pin 3 */
+#define NRF_FUN_MSPI_DQ3         83U
 
 /** @} */
 
